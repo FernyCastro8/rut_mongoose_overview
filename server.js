e79bf3a99faab79b2c54b9dea70a70bbe75b4511
JD Tadlock <jt7903@gmail.com>
2023-04-25 10:02:35 -0400

    update

--- server.js
+++ server.js
@@ -9,3 +9,8 @@ app.use(express.static('public'));
 
 app.listen(PORT, () => console.log('Started on port %s', PORT));
 
+
+const str = 'This is a string of abc10013123121321313 characters with a name JD-10';
+
+console.log(str.match(/(JD)|\d+/g));
+
