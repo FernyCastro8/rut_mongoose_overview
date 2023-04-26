5ba4d0f2d96fd46543e5e6f5b411f31fbd09cc2a
JD Tadlock <jt7903@gmail.com>
2023-04-26 13:04:16 -0400

    setup main

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
 const express = require('express');
+
 const PORT = 3000;
 
 const app = express();
 
-app.use(express.static('public'));
-
-// POST Route triggered by a button
 
 app.listen(PORT, () => console.log('Started on port %s', PORT));
-
-
-const str = 'This is a string of abc10013123121321313 characters with a name JD-10';
-
-console.log(str.match(/(JD)|\d+/g));
 
