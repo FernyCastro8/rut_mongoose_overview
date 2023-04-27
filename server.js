069af3b88b7b7b639c3450919d78c05b87aa9f5c
JD Tadlock <jt7903@gmail.com>
2023-04-27 11:12:45 -0400

    completed intro

--- server.js
+++ server.js
@@ -1,6 +1,29 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const PORT = 3000;
+
+mongoose.connect('mongodb://127.0.0.1:27017/turtles_mongoose');
+
+const Turtle = mongoose.model('turtle', {
+  type: String,
+  color: String
+});
+
+Turtle.create({
+  type: 'snapping',
+  color: 'brown and cute'
+}).then(new_turtle => {
+  console.log(new_turtle);
+});
+
+// const turtle = new Turtle({
+//   type: 'box',
+//   color: 'brown and orange'
+// });
+
+// turtle.save().then((new_turtle) => {
+//   console.log(new_turtle);
+// });
 
 const app = express();
 
